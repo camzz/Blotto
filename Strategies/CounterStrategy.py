f8cacdd0a5ae5f517afd1d7264194470ffe41cfe
Camzzz <cameronrdobbs@gmail.com>
2015-03-03 12:05:08 +0000

    Update CounterStrategy.py

--- Strategies/CounterStrategy.py
+++ Strategies/CounterStrategy.py
@@ -6,31 +6,25 @@ from StrategyUtils.random_utils import random_draw_allocation
 
 
 class CounterStrategy(BaseStrategy):
+    """
+    Counter the last move of the opponent, always beats a static strategy!
+    will apply 'counter_allocation' function to oppoents last strategy N times, where N = counte_level, default 1
+    """
     def __init__(self, name, counter_level=1):
         super(CounterStrategy, self).__init__(name)
         self.counter_level = counter_level
-        self.ready = False
-
-    def __repr__(self):
-        return "CounterStrategy counter_level %s" % self.counter_level
-
-    def initialise(self, num_fields, num_runs):
-        super(CounterStrategy, self).initialise(num_fields, num_runs)
 
     def soldiers_request(self, iteration):
-        if not self.ready:
+        if iteration == 0:
+            #Nothing to copy yet
             return random_draw_allocation([1.0 / self.num_fields for _ in xrange(self.num_fields)])
 
         else:
             alloc = self.opponent_allocations[-1]
             for _ in xrange(self.counter_level):
+                #Aha! We can beat the strategy which beats the strategy which will beat out opponent!
+                #They wont catch me out, so clever!*
                 alloc = counter_allocation(alloc)
             return alloc
-
-    def post_results(self, score, soldiers_B, check_B):
-        super(CounterStrategy, self).post_results(score, soldiers_B, check_B)
-        self.ready = True
-
-    def wipe_history(self):
-        super(CounterStrategy, self).wipe_history()
-        self.ready = False
+        
+# *not necessarily clever
