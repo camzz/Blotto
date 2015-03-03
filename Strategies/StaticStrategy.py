bd337c576b15fe948f194ca0215f46cfd9e9ba69
Camzzz <cameronrdobbs@gmail.com>
2015-03-03 12:08:29 +0000

    Simplified StaticStrategy
    Was trying to be too clever before so 1 strategy could work on any number of fields, really not necessary!

--- Strategies/StaticStrategy.py
+++ Strategies/StaticStrategy.py
@@ -13,24 +13,11 @@ class StaticStrategy(BaseStrategy):
     """
     def __init__(self, name, static_soldiers, shuffle=False):
         super(StaticStrategy, self).__init__(name)
-        if isinstance(static_soldiers, dict):
-            self.static_soldiers_map = static_soldiers
-        else:
-            self.static_soldiers_map = {len(static_soldiers): static_soldiers}
-
+        self.static_soldiers = static_soldiers
         self.shuffle = shuffle
-
-        self.static_strategy = None
-
-    def __repr__(self):
-        return "StaticStrategy: %s, shuffle:%s" % (self.static_soldiers_map, self.shuffle)
-
-    def initialise(self, num_fields, num_runs):
-        super(StaticStrategy, self).initialise(num_fields, num_runs)
-        self.static_strategy = self.static_soldiers_map[num_fields]
 
     def soldiers_request(self, iteration):
         if self.shuffle:
-            return sorted(self.static_strategy, key=lambda k: random.random())
+            return sorted(self.static_soldiers, key=lambda k: random.random())
         else:
-            return self.static_strategy
+            return self.static_soldiers
