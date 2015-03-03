8aba710ac2d662d82e5a6bf2926a46a906eece68
Lopsy <linyks@gmail.com>
2015-03-03 17:32:46 +0100

    Delete __init__.py

--- Strategies/__init__.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'camzzz'
-
-from RandomStrategy import RandomStrategy
-from StaticStrategy import StaticStrategy
-from MixedStrategy import MixedStrategy
-from CounterStrategy import CounterStrategy
-
-__all__ = ['RandomStrategy', 'StaticStrategy', 'MixedStrategy', 'CounterStrategy']
