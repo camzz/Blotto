eab89735a37ff154d0baf890f567ba4c3b7fb0ac
camzz <cameronrdobbs@gmail.com>
2015-03-06 21:22:04 +0000

    CamzNewStrat

--- scripts/beat_scroggs.py
+++ scripts/beat_scroggs.py
@@ -7,15 +7,15 @@ from Strategies.Scroggs import Scroggs
 from Strategies.RandomStrategy import RandomStrategy, RandomStyle
 from Strategies.MixedStrategy import MixedStrategy
 
-c = CamzLearner('camz', '/home/cameron/Code/Blotto/data/beat_scroggs.json', keep_fct=1.0, add_fct=1.0)
-s = Scroggs('s')
+c = CamzLearner('camz', '/home/cameron/Code/Blotto/data/beat_unif.json', keep_fct=1.0, add_fct=1.0)
+s = Scroggs('scroggs')
 r = RandomStrategy('r', style=RandomStyle.UNIFORM)
 m = MixedStrategy('scroggs_and_unif', [s, r], [0.9, 0.1])
 
 
-gm = GameManager(m, c, 8, 3000)
+gm = GameManager(r, c, 8, 10000)
 
-gm.run()
+gm.run()gt
 gm.plot_results()
 gm.declare_winner()
 
